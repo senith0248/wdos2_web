1feac14705163037ee07ef1cba3d76a42378f012
senith0248 <153298308+senith0248@users.noreply.github.com>
2024-04-09 16:11:27 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 document.getElementById("login_Form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent form submission
+    event.preventDefault(); 
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
     authenticateUser(username, password);
@@ -16,18 +16,18 @@ function authenticateUser(username, password) {
         .then(data => {
             const user = data.user.find(user => user.username === username && user.password === password);
             if (user) {
-                // Login successful
+             
                 document.getElementById("loginMessage").textContent = "Login successful!";
-                // Redirect to dashboard based on user role
+               
                 if (user.role === 'admin') {
-                    // Redirect to admin dashboard
+                   
                     window.location.href = "dashboard.html";
                 } else if (user.role === 'site_user') {
-                    // Redirect to site user dashboard or homepage
+                   
                     window.location.href = "index.html";
                 }
             } else {
-                // Login failed
+            
                 document.getElementById("loginMessage").textContent = "Invalid username or password.";
             }
         })
